0088fb8593d7ed9db29030faaf739a69155d9850
JasNjoku <=>
2025-11-03 15:11:31 +0000

    fix sql distinct eror

--- java-school-database/src/main/java/mthree/com/SchoolDatabase/dao/SchoolDaoImpl.java
+++ java-school-database/src/main/java/mthree/com/SchoolDatabase/dao/SchoolDaoImpl.java
@@ -77,7 +77,7 @@ public class SchoolDaoImpl implements SchoolDao {
         // Name the aggregate field `numStudents`.
         // YOUR CODE STARTS HERE
 
-        String sql = "SELECT course.courseCode, course.courseDesc, COUNT(course_student.student_id) AS numStudents " +
+        String sql = "SELECT course.courseCode, course.courseDesc, COUNT(DISTINCT course_student.student_id) AS numStudents " +
                 "FROM course LEFT JOIN course_student ON course.cid = course_student.course_id " +
                 "GROUP BY course.courseCode, course.courseDesc";
 
