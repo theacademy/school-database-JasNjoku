adbbfbe9deece94482f3891a65eeb59c541cccb9
JasNjoku <=>
2025-11-03 17:20:54 +0000

    rewrite students per class

--- java-school-database/src/main/java/mthree/com/SchoolDatabase/dao/SchoolDaoImpl.java
+++ java-school-database/src/main/java/mthree/com/SchoolDatabase/dao/SchoolDaoImpl.java
@@ -77,9 +77,11 @@ public class SchoolDaoImpl implements SchoolDao {
         // Name the aggregate field `numStudents`.
         // YOUR CODE STARTS HERE
 
-        String sql = "SELECT course.courseCode, course.courseDesc, COUNT(course_student.student_id) AS numStudents " +
-                "FROM course LEFT JOIN course_student ON course.cid = course_student.course_id " +
-                "GROUP BY course.courseCode, course.courseDesc " +
+        String sql = "SELECT course.cid, course.courseCode, course.courseDesc, " +
+                "COUNT(course_student.student_id) AS numStudents " +
+                "FROM course " +
+                "LEFT JOIN course_student ON course.cid = course_student.course_id " +
+                "GROUP BY course.cid, course.courseCode, course.courseDesc " +
                 "ORDER BY course.courseCode";
 
         // YOUR CODE ENDS HERE
